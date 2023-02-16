f6ffd0676dc607feb407264bcd5345032f146ae0
BIGBASEDFLOPPA <102544286+BIGBASEDFLOPPA@users.noreply.github.com>
2023-02-16 21:02:21 +0300

    Update main.js

--- main.js
+++ main.js
@@ -5,11 +5,11 @@ let rightButton = document.querySelector('#right');
 
 leftButton.addEventListener('click', function(event){
     let goLeft = box.offsetLeft;
-    box.style.left = goLeft - 40 + "px";
+    box.style.left = goLeft - 50 + "px";
 });
 rightButton.addEventListener('click', function(event){
     let goRight = box.offsetLeft;
-    box.style.left = goRight + 40 + "px";
+    box.style.left = goRight + 50 + "px";
 });
 box.addEventListener("click", function() {
     this.classList.toggle("is-dropped");
