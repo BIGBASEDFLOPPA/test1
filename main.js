aaa4f84ab7dbc4571d8f745e7d0f2e751c3f7b27
Alexandr Logvinov <begemott4@gmail.com>
2023-02-16 21:49:42 +0300

    ready

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
-let box = document.querySelector('.box');
-let leftButton = document.querySelector('#left');
-let rightButton = document.querySelector('#right');
+const box = document.querySelector('.box');
+const leftButton = document.getElementById('left');
+const rightButton = document.getElementById('right');
 
 
-leftButton.addEventListener('click', function(event){
-    let goLeft = box.offsetLeft;
-    box.style.left = goLeft - 50 + "px";
+leftButton.addEventListener('click', function (event) {
+    const goLeft = box.offsetLeft;
+    const merge = goLeft - 100;
+    if (merge >= 0)
+        box.style.left = merge + 'px';
 });
-rightButton.addEventListener('click', function(event){
-    let goRight = box.offsetLeft;
-    box.style.left = goRight + 50 + "px";
+rightButton.addEventListener('click', function (event) {
+    const goRight = box.offsetLeft;
+    const merge = goRight + 100;
+    if (window.screen.width >= merge)
+        box.style.left = merge + 'px';
 });
-box.addEventListener("click", function() {
-    this.classList.toggle("is-dropped");
+box.addEventListener('click', function () {3
+    this.classList.toggle('is-dropped');
 })
 
 
